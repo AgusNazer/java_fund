4ff2e8c0943e04ce828bbd9a09058fdcd82efd72
agusNazer <agustin.nazer@hotmail.com>
2024-12-08 17:25:34 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,43 @@
+// public class App {
+//     public static void main(String[] args) throws Exception {
+//         System.out.println("Hello, World!");
+
+//         // variables es muy muy similar a c++
+//          int numero = 5;
+//          System.out.println(numero);
+//     }
+
+// }
+
+// ejemplo de bvariable class string
+public class App {
+    public static void main(String[] args) throws Exception {
+    //    String texto = "Este es un texto asignado a una variable a String";
+    //    System.out.println(texto);
+
+    //    int longitud = texto.length();
+    //    System.out.println(longitud);
+
+    //    String substring = texto.substring(6, 16);
+    //    System.out.println(substring);
+
+    //    String minuscula = texto.toLowerCase();
+    //    System.out.println(minuscula);
+
+    //    String upperCase = texto.toUpperCase();
+    //    System.out.println(upperCase);
+
+    //    boolean contiene = texto.contains("pepe");
+    //    System.out.println("Tiene la palabra pepe?: " + contiene);
+       for (int i = 1; i<=10; i++){
+        System.out.println(i);
+     
+     }
+     int[] numeros = new int[3];
+     System.out.println(numeros[2]);
+
+     }
+     
+ }
+ 
+
